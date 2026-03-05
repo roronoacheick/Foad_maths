f161369af7b3a6c170aca736e1fb95e82c09958b
roronoacheick <roronoacheick3@icloud.com>
2026-03-05 11:54:25 +0100

    fearure: add the function quadratic_regression

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def quadratic_regression(a, b, c, x):
+    return a * x**2 + b * x + c
+# c'est plus flexible qu'un modèle affine car il peut s'adapter  a plusieurs types de données et en plus c'est une parabole 
 if __name__ == "__main__":
     house_prices_df = pd.read_csv("prix_maisons.csv")
     x_mean, x_std= house_prices_df["surface"].mean(), house_prices_df["surface"].std()
@@ -13,7 +16,6 @@ if __name__ == "__main__":
     print(house_prices_df.head(5))
     print(house_prices_df.dtypes)
     print(house_prices_df.shape)
-
     #Questions
     # Pourquoi la standardisation aide la descente de gradient ?
     #La standardisation aide la descente de gradient parce qu'elle rend toutes les variables à la même échelle du coup sa converge plus vite 
@@ -21,3 +23,10 @@ if __name__ == "__main__":
     #     #Que se passe-t-il si on ne normalise pas et que les surfaces sont en dizaines alors que les prix
     # sont en centaines de milliers ?
     #sa vas converger de manière très lente le gradient pour le prix sera énorme 
+
+    plt.scatter(house_prices_df["surface"], house_prices_df["prix"])
+    plt.xlabel("surface")
+    plt.ylabel("prix")
+    plt.show()
+
+    
